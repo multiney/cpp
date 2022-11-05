6fe43626b41d29de211fa3fb003ec6c7134d8e44
vk <vk@multi.com>
2022-11-05 17:11:32 +0800

    array

--- DataStruct/Array/array.cpp
+++ DataStruct/Array/array.cpp
@@ -1,5 +1,7 @@
+#include <climits>
 #include <malloc.h>
 
+#include <unordered_map>
 #include <vector>
 #include <string>
 
@@ -220,9 +222,9 @@ void moveZeroes2(vector<int> &nums) {
 /*
  * 844. Backspace String Compare
  */
-bool BackspaceCompare(string s, string t) {
+bool backspaceCompare(string s, string t) {
     int i = s.size() - 1, j = t.size() - 1;
-    while (i >= 0 && j >= 0) {
+    while (true) {
         int count = 0;
         while (i >= 0) {
             if (s[i] == '#')
@@ -233,6 +235,180 @@ bool BackspaceCompare(string s, string t) {
                 break;
             --i;
         }
-        if (i == 0)
+        count = 0;
+        while (j >= 0) {
+            if (t[j] == '#')
+                ++count;
+            else if (count > 0)
+                --count;
+            else if (count == 0)
+                break;
+            --j;
+        }
+        if (i >= 0 && j >= 0 && s[i] == t[j])   {
+            --i;
+            --j;
+        } else {
+            break;
+        }
     }
+    return i == -1 && j == -1;
+}
+bool backspaceCompare2(string s, string t) {
+    int slowS = 0, slowT = 0;
+    for (int fast = 0; fast < s.size(); ++fast) {
+        if (s[fast] != '#')
+            s[slowS++] = s[fast];
+        else if (slowS > 0)
+            --slowS;
+    }
+    for (int fast = 0; fast < t.size(); ++fast) {
+        if (t[fast] != '#')
+            t[slowT++] = t[fast];
+        else if (slowT > 0)
+            --slowT;
+    }
+    if (slowS != slowT) return false;
+    for (int i = 0; i < slowS; ++i)
+        if (s[i] != t[i]) return false;
+    return true;
+}
+
+/*
+ * 977. Squares of a Sorted Array
+ *
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* sortedSquares(int* nums, int numsSize, int* returnSize){
+    int left = 0, right = numsSize - 1, pos = right;
+    int *ret = (int*)malloc((*returnSize = numsSize) * sizeof(int));
+    while (left < right) {
+        nums[left] = nums[left] < 0 ? -nums[left] : nums[left];
+        nums[right] = nums[right] < 0 ? -nums[right] : nums[right];
+        if (nums[left] < nums[right]) {
+            ret[pos--] = nums[right] * nums[right];
+            --right;
+        } else {
+            ret[pos--] = nums[left] * nums[left];
+            ++left;
+        }
+    }
+    ret[pos] = nums[left] * nums[left];
+    return ret;
+}
+/*
+ * -------------------------------------------
+ * 27=========================================
+ */
+
+/*
+ * 209=========================================
+ * -------------------------------------------
+ */
+
+/*
+ * 209. Minimum Size Subarray Sum
+ */
+int minSubArrayLen(int target, vector<int> &nums) {
+    int left = 0, sum = 0, minLen = INT_MAX;
+    for (int right = 0; right < nums.size(); ++right) {
+        sum += nums[right];
+        while (sum >= target) {
+            minLen = right - left + 1 < minLen ? right - left + 1 : minLen;
+            sum -= nums[left++];
+        }
+    }
+    return minLen == INT_MAX ? 0 : minLen;
+}
+
+int minSubArrayLen2(int target, vector<int> &nums) {
+    int left = 0, minLen = INT_MAX;
+    for (int right = 0; right < nums.size(); ++right) {
+        target -= nums[right];
+        while (target <= 0) {
+            minLen = right - left + 1 < minLen ? right - left + 1 : minLen;
+            target += nums[left++];
+        }
+    }
+    return minLen = INT_MAX ? 0 : minLen;
+}
+int upperBound(int begin, int end, int target, const vector<int> &vec) {
+    int mid;
+    while (begin < end) {
+        mid = begin + ((end - begin) >> 1);
+        if (vec[mid] > target)
+            end = mid;
+        else
+            begin = mid + 1;
+    }
+    return begin;
+}
+int minSubArrayLen3(int target, vector<int> &nums) {
+    int n = nums.size(), minLen = INT_MAX;
+    vector<int> sums(n + 1);
+    for (int i = 1; i <= n; ++i)
+        sums[i] = nums[i - 1] + sums[i - 1];
+    for (int i = n; i > 0 && sums[i] >= target; --i) {
+        int j = upperBound(0, i, sums[i] - target, sums);
+        minLen = std::min(minLen, i - j + 1);
+    }
+    return minLen == INT_MAX ? 0 : minLen;
+}
+
+/*
+ * 904. Fruit Into Baskets
+ */
+int totalFruit(vector<int> &fruits) {
+    std::unordered_map<int, int> count;
+    int i, j;
+    for (i = 0, j = 0; j < fruits.size(); ++j) {
+        ++count[fruits[j]];
+        if (count.size() > 2) {
+            if (--count[fruits[i]] == 0)
+                count.erase(fruits[i]);
+        ++i;
+        }
+    }
+    return j - i;
+}
+int totalFruit2(vector<int> &fruits) {
+    int left = 0, right = 0, ret = 0;
+    int ln = fruits[left], rn = fruits[right];
+    while (right < fruits.size()) {
+        if (fruits[right] == ln || fruits[right] == rn) {
+            ret = std::max(ret, right - left + 1);
+            ++right;
+        } else {
+            left = right - 1;
+            ln = fruits[left];
+            while (left > 0 && fruits[left - 1] == ln) --left;
+            rn = fruits[right];
+        }
+    }
+    return ret;
+}
+
+int lengthOfLongestSubstringTwoDistinct(string s) {
+    vector<int> map(128, 0);
+    int counter = 0, begin = 0, end = 0, ret = 0;
+    while (end < s.size()) {
+        if (map[s[end++]]++ == 0) ++counter;
+        while (counter > 2) if (map[s[begin++]]-- == 1) --counter;
+        ret = std::max(ret, end - begin);
+    }
+    return ret;
+}
+
+/*
+ * 3. Longest Substring Without Repeating Characters
+ */
+int lenthOfLongestSubstring(string s) {
+    vector<int> map(128, 0);
+    int counter = 0, begin = 0, end = 0, ret = 0;
+    while (end < s.size()) {
+        if (map[s[end++]]++ > 0) ++counter;
+        while (counter > 0) if (map[s[begin++]]-- > 1) --counter;
+        ret = std::max(ret, end - begin);
+    }
+    return ret;
 }
